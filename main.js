b63204c8b9e94613bc7a6b3a16fa0526fac27f4d
Ojoachele Onuh <ejigboojoachele@gmail.com>
2024-02-11 16:22:52 +0100

    Add futher validations for card number

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 /** PSEUDOCODE
- *
+ * check that the card number contains only digit
+ * check the length
+ * check the card type
  */
 
 function validateCreditCard(cardNumber) {
@@ -19,8 +21,31 @@ function validateCreditCard(cardNumber) {
   } else {
     return "Unsupported card type. Please enter a Visa or Mastercard.";
   }
+
+  let sum = 0;
+  let double = false;
+
+  for (let i = cleanedCardNumber.length - 1; i >= 0; i--) {
+    let digit = parseInt(cleanedCardNumber.charAt(i));
+
+    if (double) {
+      digit *= 2;
+      if (digit > 9) {
+        digit -= 9;
+      }
+    }
+
+    sum += digit;
+    double = !double;
+  }
+
+  if (sum % 10 !== 0) {
+    return "Invalid card number. Please enter a valid credit card number.";
+  }
+
+  return `Valid ${cardType} card number.`;
 }
 
-const cardNumber = "1234-5678-9012-3452"; // Replace with the actual card number
+const cardNumber = "4187-4517-3951-1308"; // Replace with the actual card number
 const validationMessage = validateCreditCard(cardNumber);
 console.log(validationMessage);
